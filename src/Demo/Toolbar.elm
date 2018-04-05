06631c0522502f62b1e55d453e8a5dcac2e85f37
Aron Wouters <firestark@mail.com>
2018-04-05 17:18:39 +0200

    Toolbar demo

--- src/Demo/Toolbar.elm
+++ src/Demo/Toolbar.elm
@@ -1,16 +1,32 @@
 module Demo.Toolbar exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, id)
 import Material.Attributes exposing (..)
 import Material.Button exposing (iconbutton)
 
 
 standard : Html msg
 standard =
-    div [ class "app-bar", toolbar, elevation 2 ]
-        [ section []
-            [ iconbutton [] "menu"
-            , h1 [ title ] [ text "Title" ]
+    div [ id "toolbar-demo" ]
+    [ section []
+        [ h2 [ headline ] [ text "Single row" ]
+        , div [ class "app-bar", toolbar, elevation 2 ]
+            [ section []
+                [ iconbutton [] "menu"
+                , h1 [ title ] [ text "Title" ]
+                ]
             ]
         ]
+    , section []
+        [ h2 [ headline ] [ text "Double row" ]
+        , div [ class "app-bar", toolbar, elevation 2 ]
+            [ section []
+                [ iconbutton [] "menu"
+                , h1 [ title ] [ text "Title" ]
+                ]
+            , section [] []
+            ]
+        ]
+    ]
+
